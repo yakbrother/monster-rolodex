e83cda855f41927514a130885a5604a84de6cae9
Tim Eaton <tim@reports-inc.com>
2023-04-21 13:56:15 +0200

    More styles & bugfix

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,17 @@
 import { Component } from 'react';
 
+import './card-list.styles.css';
+
+import Card from '../card/card.component';
+
 class CardList extends Component {
     render() {
         const { monsters } = this.props;
 
         return (
-            <div>
+            <div className='card-list'>
                 {monsters.map((monster) => {
-                    return <h1 key={monster.id}>{monster.name}</h1>
+                    return <Card monster={monster} />
                 })}
             </div>
         );
